feb90de0b7b5814a86f10ee8c047024a5ca7b69c
KelvenVS <kelven.serejo@gmail.com>
2023-01-20 16:16:16 -0300

    ArrayInObject

---
+++ class2/types.js
@@ -0,0 +1,25 @@
+const client = {
+    name: "João",
+    age: 24,
+    email: "João@firma.com",
+    tel: ["1193546871", "1194863514"]
+};
+
+
+const estudante = {
+    nome: "Adilson Josué de Freitas",
+    matricula: 201542290,
+    curso: "Engenharia eletrônica",
+    materias: [
+        "Cálculo 1",
+        "Álgebra Linear",
+        "Práticas de Laboratório",
+        "Metodologia",
+    ],
+};
+
+// console.log(client);
+
+console.log(client.tel[1]);
+
+console.log(estudante);
