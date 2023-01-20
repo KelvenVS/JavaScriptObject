0ffb50167ce00fdab789b8a98d868840cc0bfa26
KelvenVS <kelven.serejo@gmail.com>
2023-01-20 16:22:17 -0300

    Objetos em Objetos

---
+++ class2/nested.js
@@ -0,0 +1,15 @@
+const client = {
+    name: "João",
+    age: 24,
+    email: "João@firma.com",
+    tel: ["1193546871", "1194863514"]
+};
+
+client.adress = {
+    rua: "R. Jojo Climber",
+    number: 1548,
+    apartment: true,
+    complement: "ap 74",
+};
+
+console.log(client);
