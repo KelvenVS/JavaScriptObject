d2218d65d1c8c46f5a2bb9e800ddbbbf605be602
KelvenVS <kelven.serejo@gmail.com>
2023-01-20 16:48:52 -0300

    FunctionsInObject ObjectCopy

--- class2/functions.js
+++ class2/functions.js
@@ -17,3 +17,15 @@ const client = {
 
 client.makePayment(250);
 client.makePayment(175);
+
+const objPersonagem = {
+    nome: "Gandalf",
+    classe: "mago",
+    nivel: "20"
+   }
+   
+   const objPersonagem2 = Object.create(objPersonagem)
+   objPersonagem2.nome = "Gandalf, o Cinzento"
+   
+   console.log(objPersonagem.nome) //Gandalf
+   console.log(objPersonagem2.nome) //Gandalf, o Cinzento
