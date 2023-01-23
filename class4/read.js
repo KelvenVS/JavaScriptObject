69908428a594af3160e8ff6aefb018f56ae928cb
KelvenVS <kelven.serejo@gmail.com>
2023-01-23 19:56:49 -0300

    Read Object // turn into string // turn into JSON

--- class4/read.js
+++ class4/read.js
@@ -1,4 +1,20 @@
 const data = require("./client.json");
 
-console.log(data);
+console.log(data.numero); //undefined
+console.log(data.endereco.numero);
+console.log(data.nome);
 console.log(typeof data);
+
+//turn into string
+const clientInString = JSON.stringify(data);
+
+console.log(clientInString);
+console.log(typeof clientInString);
+
+console.log(clientInString.name); //undefined
+
+//turn into JSON
+const objectClient = JSON.parse(clientInString);
+
+console.log(objectClient);
+console.log(typeof objectClient);
